7420ba063c3f4bcb4910fe04146f7184f247936d
Michealbori <moluwabori448@gmail.com>
2024-03-01 16:37:41 +0100

    Cpcos_Landing-Page

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+function Home() {
+    window.alert("YOU ARE CURRENTLY IN THE HOME SECTION, DO YOU HAVE ANYTHING YOU WHAT TO TELL US?");
+}
+
+function About() {
+    window.alert("THE PAGE IS NOT YET READY");
+}
+
+function Pricing() {
+    window.alert("THE PAGE IS NOT YET READY");
+}
+
+function Contact() {
+    window.alert("THE PAGE IS NOT YET READY");
+}
+
+function ContactUSbtn() {
+    window.alert("THE PAGE IS NOT YET READY");
+}
+
+function GetStartedbtn(){
+    window.alert("THE PAGE IS NOT YET READY");
+}
